5925fa12fa545cbfd20bca92367d5ca5be0cddd9
Ubuntu <ubuntu@ip-172-31-2-183.us-west-2.compute.internal>
2022-11-12 08:09:11 +0000

    Intermediate update

--- src/models/mnist_module.py
+++ src/models/mnist_module.py
@@ -58,18 +58,18 @@ class MNISTLitModule(LightningModule):
     def forward(self, x: torch.Tensor):
         return self.net(x)
     
-    @torch.jit.export
-    def forward_jit(self, x: torch.Tensor):
-        with torch.no_grad():
-            # transform the inputs
-            x = self.predict_transform(x)
+    # @torch.jit.export
+    # def forward_jit(self, x: torch.Tensor):
+    #     with torch.no_grad():
+    #         # transform the inputs
+    #         x = self.predict_transform(x)
 
-            # forward pass
-            logits = self(x)
+    #         # forward pass
+    #         logits = self(x)
 
-            preds = F.softmax(logits, dim=-1)
+    #         preds = F.softmax(logits, dim=-1)
 
-        return preds
+    #     return preds
 
     def on_train_start(self):
         # by default lightning executes validation step sanity checks before training starts,
